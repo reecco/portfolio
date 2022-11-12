b220558aeabe002eed905316c15accdd136d77bd
reecco <fredcardoso01@hotmail.com>
2022-11-12 13:03:16 -0300

    fix: change message of field

--- src/components/ContactForm/ContactForm.vue
+++ src/components/ContactForm/ContactForm.vue
@@ -29,7 +29,7 @@ export default {
   methods: {
     async sendEmail(event) {
       event.preventDefault()
-      const alert = 'Fill in the fields correctly, the fields in red are mandatory.'
+      const alert = 'Fill in the fields correctly.'
 
       let emailInput = document.getElementById('input-email')
       let nameInput = document.getElementById('input-name')
