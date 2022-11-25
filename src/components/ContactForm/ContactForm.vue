200afead112c7e10bd15904c875813fdd3560106
reecco <fredcardoso01@hotmail.com>
2022-11-25 13:27:35 -0300

    feat: add modal box in contact form

--- src/components/ContactForm/ContactForm.vue
+++ src/components/ContactForm/ContactForm.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="contact__form">
+    <div id="vis-modal" class="modal">
+      <div class="conteudo-modal">
+        <div class="body-modal">
+          <p class="message">{{ alert }}</p>
+          <button class="btn-outline-success" @click="closeModal('vis-modal')">Close</button>
+        </div>
+      </div>
+    </div>
     <h2 class="contact__form-title">Send a message</h2>
     <form @submit="sendEmail">
-      <p class="message">{{ alert }}</p>
+      <!-- <p class="message">{{ alert }}</p> -->
       <input type="text" name="email" class="input input-email" id="input-email" placeholder="Email" v-model="email">
       <input type="text" name="name" class="input input-name" id="input-name" placeholder="Name" v-model="name">
-      <textarea type="text" name="text" class="textarea" id="textarea-text" placeholder="Message" v-model="text"></textarea>
+      <textarea type="text" name="text" class="textarea" id="textarea-text" placeholder="Message"
+        v-model="text"></textarea>
       <button class="btn-send">Send</button>
     </form>
   </div>
@@ -46,22 +55,26 @@ export default {
 
       if (email) {
         emailInput.style.border = '2px solid red'
-        
-        setTimeout(() => emailInput.style.border = '' , 5000)
+
+        setTimeout(() => emailInput.style.border = '', 5000)
       } else if (name) {
         nameInput.style.border = '2px solid red'
 
-        setTimeout(() => nameInput.style.border = '' , 5000)
+        setTimeout(() => nameInput.style.border = '', 5000)
       } else if (text) {
         textArea.style.border = '2px solid red'
 
-        setTimeout(() => textArea.style.border = '' , 5000)
+        setTimeout(() => textArea.style.border = '', 5000)
       } else {
         const res = await api(this.email, this.name, this.text)
 
         if (res.status == 201) {
           this.alert = 'Message sent successfully!'
-          setTimeout(() => this.alert = '', 5000)
+          this.openModal('vis-modal')
+          setTimeout(() => {
+            this.alert = ''
+            this.closeModal('vis-modal')
+          }, 10000)
         } else {
           this.alert = res.response.data.message
           setTimeout(() => this.alert = '', 4000)
@@ -71,6 +84,22 @@ export default {
         this.name = ''
         this.text = ''
       }
+    },
+
+    openModal(loadModal) {
+      let modal = document.getElementById(loadModal)
+
+      modal.style.display = 'block'
+
+      document.body.style.overflow = 'hidden'
+    },
+
+    closeModal(closeModal) {
+      let modal = document.getElementById(closeModal)
+
+      modal.style.display = 'none'
+
+      document.body.style.overflow = 'auto'
     }
   }
 }
