74740ee4790d72e16422bc2e13ac54b075128512
reecco <fredcardoso01@hotmail.com>
2022-11-25 21:19:23 -0300

    fix: refactoring contact form

--- src/components/ContactForm/ContactForm.vue
+++ src/components/ContactForm/ContactForm.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="contact__form">
     <div id="vis-modal" class="modal">
-      <div class="conteudo-modal">
+      <div class="content-modal">
         <div class="body-modal">
-          <p class="message">{{ alert }}</p>
+          <p class="message-success">{{ success }}</p>
           <button class="btn-outline-success" @click="closeModal('vis-modal')">Close</button>
         </div>
       </div>
     </div>
     <h2 class="contact__form-title">Send a message</h2>
+    <p class="message-error">{{ error }}</p>
     <form @submit="sendEmail">
-      <!-- <p class="message">{{ alert }}</p> -->
       <input type="text" name="email" class="input input-email" id="input-email" placeholder="Email" v-model="email">
       <input type="text" name="name" class="input input-name" id="input-name" placeholder="Name" v-model="name">
       <textarea type="text" name="text" class="textarea" id="textarea-text" placeholder="Message"
@@ -31,53 +31,44 @@ export default {
       email: '',
       name: '',
       text: '',
-      alert: ''
+      success: '',
+      error: ''
     }
   },
 
   methods: {
     async sendEmail(event) {
       event.preventDefault()
-      const alert = 'Fill in the fields correctly.'
-
-      let emailInput = document.getElementById('input-email')
-      let nameInput = document.getElementById('input-name')
-      let textArea = document.getElementById('textarea-text')
 
       let email = this.email == '' || this.email == ' '
       let name = this.name == '' || this.name == ' '
       let text = this.text == '' || this.text == ' '
 
       if (email || name || text) {
-        this.alert = alert
-        setTimeout(() => this.alert = '', 5000)
-      }
+        this.error = 'Fill in the fields correctly.'
+        setTimeout(() => this.error = '', 5000)
 
-      if (email) {
-        emailInput.style.border = '2px solid red'
-
-        setTimeout(() => emailInput.style.border = '', 5000)
-      } else if (name) {
-        nameInput.style.border = '2px solid red'
-
-        setTimeout(() => nameInput.style.border = '', 5000)
-      } else if (text) {
-        textArea.style.border = '2px solid red'
-
-        setTimeout(() => textArea.style.border = '', 5000)
+        if (email) {
+          this.styleInput('input-email')
+        } 
+        if (name) {
+          this.styleInput('input-name')
+        } 
+        if (text) {
+          this.styleInput('textarea-text')
+        }
       } else {
         const res = await api(this.email, this.name, this.text)
 
         if (res.status == 201) {
-          this.alert = 'Message sent successfully!'
+          this.success = 'Message sent successfully!'
           this.openModal('vis-modal')
           setTimeout(() => {
-            this.alert = ''
             this.closeModal('vis-modal')
           }, 10000)
         } else {
-          this.alert = res.response.data.message
-          setTimeout(() => this.alert = '', 4000)
+          this.error = res.response.data.message
+          setTimeout(() => this.error = '', 4000)
         }
 
         this.email = ''
@@ -100,6 +91,12 @@ export default {
       modal.style.display = 'none'
 
       document.body.style.overflow = 'auto'
+    },
+
+    styleInput(id) {
+      document.getElementById(id).style.border = '2px solid red'
+
+      setTimeout(() => document.getElementById(id).style.border = '', 5000)
     }
   }
 }
